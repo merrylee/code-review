040056e90c1d659d919cff283e75f21bfef52136
seonmin <ismpink@daum.net>
2019-04-15 17:26:31 +0900

    github user api 적용

--- projects/App.jsx
+++ projects/App.jsx
@@ -1,39 +1,82 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link, NavLink, Switch } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 
-const Home = () => <h1>Home</h1>;
+class Profile extends React.Component {
+ 
+    state = { userInfo: '' };
 
-const Links = () => (
-    <nav>
-        <Link to="/">Home</Link>
-        <Link to="/menu">Menu</Link>
-    </nav>
-);
+    componentDidMount() {
+        const { onLoading, match } = this.props;
+        const userId = match.params.profileId;
 
-const Menu = () => (
-    <div>
-         <h1>Menu</h1>
-         <nav>
-            <Link to="/menu/food">Food</Link>
-            <Link to="/menu/drinks">Drinks</Link>
-            <Link to="/menu/sides">Sides</Link>
-        </nav>
-        <Route
-                path="/:page?/:subpage?"
-                render={({ match }) => <h1>{match.params.subpage}</h1>} />
-    </div>
-)
+        onLoading(true);
+        fetch(`https://api.github.com/users/${userId}`)
+            .then(res => res.json())
+            .then(data => {
+                onLoading(false);
+                this.setState({userInfo: data}); 
+            });
+    }
 
-const App = () => (
-    <Router>
-        <div>
-            <Links />
-            <Route exact path="/" component={Home} />
-            <Route path="/menu" component={Menu} />
-        </div>
-    </Router>
-);
+    render() {
+        const { loading } = this.props;
+        const { userInfo } = this.state;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+        if(loading) return <div>정보를 불러오는 중입니다...</div>;
+
+        return (
+            <div>
+                <div>프로필 정보: {userInfo.login}</div>
+                <div><img alt="" width="230" height="230" class="" src={userInfo.avatar_url} /></div>
+                <div>이름: {userInfo.name}</div>
+                <div>이메일: {userInfo.email === null ? '미등록' : userInfo.email}</div>
+            </div>
+        );
+    }
+};
+
+const SelectProfile = () => <div>프로필을 하나 선택하세요</div>;
+
+class App extends React.Component {
+    state = {
+        loading: false
+    };
+
+    handleLoading = (loading) => {
+        this.setState({loading});
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="links">
+                    <Link to="/profile" className="link">
+                        Name
+                    </Link>
+                    <Link to="/profile/miconblog" className="link">
+                        miconblog
+                    </Link>
+                    <Link to="/profile/merrylee" className="link">
+                        merrylee
+                    </Link>
+                </div>
+                <div className="tabs">
+                    <Switch>
+                        <Route path="/profile" exact component={SelectProfile} />
+                        <Route path="/profile/:profileId" render={(props) => {
+                            return ( <Profile key={props.location.pathname}
+                                            {...props}
+                                            loading={this.state.loading}
+                                            onLoading={this.handleLoading}
+                                    />
+                            );
+                        }} />
+                    </Switch>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
